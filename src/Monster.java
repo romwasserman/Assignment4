5e485d2d7968d6e3cb1f9228351a2c965a387b57
romwasserman <rom.wasserman@quinnipiac.edu>
2021-02-22 19:25:35 -0500

    created a new depo

---
+++ src/Monster.java
@@ -0,0 +1,32 @@
+
+public class Monster {
+	public double health,strength;
+	Monster m = new Monster();
+	
+	public Monster()
+	{
+		this.health = 0;
+		this.strength = 0;		
+	}
+
+	
+	public Monster(double newHealth, double newStrength)
+	{
+		this.health = newHealth;
+		this.strength = newStrength;
+	}
+	
+	public double getHealth() { return m.health; }	
+	public double getStrength() { return m.strength; }
+	
+	public void setHealth(double health) { this.health = health;}
+	public void setStrength(double strength) { this.strength = strength;}
+	public double attack()
+	{
+		return (Math.random() * (m.getStrength()));
+	}
+	public void takeDamage(double damage)
+	{
+		 health -= damage;
+	}
+}
